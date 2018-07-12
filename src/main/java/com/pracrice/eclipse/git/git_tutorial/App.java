3f8be13b88138c2451060e42483e27a3c70cb658
tabhishek <abhishek.trivedi@ezops.com>
2018-07-12 11:13:12 +0530

    Initial Repository Code

---
+++ src/main/java/com/pracrice/eclipse/git/git_tutorial/App.java
@@ -0,0 +1,14 @@
+package com.pracrice.eclipse.git.git_tutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("It Abhishek Trivedi Commit");
+    }
+}
